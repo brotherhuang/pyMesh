e28abac55066e7297cb06027de03ea739831e68c
haibin <jackiehuanghaibin@gmail.com>
2016-07-03 00:31:15 -0400

    fix rendering

--- meshUI.py
+++ meshUI.py
@@ -157,7 +157,7 @@ def drawMesh():
 def Draw ():
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	glTranslatef(-1.5, 0.0, -5.0);
+	gluLookAt(0.0,0.0,5.0,0.0,0.0,0.0,0.0,1.0,0.0)
 	glPushMatrix();
 	glMultMatrixf(g_Transform);
 	glColor3f(0.75,0.75,1.0)
