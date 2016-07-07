42d99b6ebaf8bdc276c599a1b4b7f415bcbf16c9
root <jackiehuanghaibin@gmail.com>
2016-07-06 23:49:36 -0400

    add find connected comp

--- meshAlgorithm.py
+++ meshAlgorithm.py
@@ -37,7 +37,20 @@ def lmsmooth(mesh, iterations):
         umbrella(mesh,-0.331,False)
 
 def remove_vertices(mesh, toremove):
+    nv = len(mesh.vertices)
+    if nv == 0: return
+    remap_table = [-1] * nv
+    next = 0
+    for i in range(nv):
+        if toremove[i] :
+            remap_table[i] = next
+            next += 1
+    if next == nv: return
+    remap_verts(mesh,remap_table)
+
+def remap_verts(mesh, remap_table):
     return
+
 
 def remove_faces(mesh, toremove):
     return
@@ -54,6 +67,30 @@ def diffuse_vector(mesh, field, sigma):
 
 def diffuse_normals(mesh, sigma):
     return
+
+
+def find_comps(mesh):
+    def dfs(comps, neighbors, f, index):
+        for i in neighbors[f]:
+            if comps[i] == -1:
+                comps[i] = index
+                dfs(comps, neighbors,i, index)
+    nf = len(mesh.faces)
+    mesh.need_adjacentfaces()
+    comps = [-1] * nf
+    compsize = []
+    for i in range(nf):
+        if comps[i] == - 1:
+            index = len(compsize)
+            comps = index
+            compsize.append[index]
+            dfs(comps, mesh.adjacentfaces, i, index)
+    for i in range(len(compsize)):
+        compsize[i] = comps.count(i)
+
+
+
+
 
 
 
